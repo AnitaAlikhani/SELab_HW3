4baf7f50b2a2dd6d7751675b2797c1460cf3f894
AnitaAlikhani <AnitaAlikhani12@gmail.com>
2024-07-28 15:50:40 +0330

    Increase test coverage for PersonValidator

---
+++ src/test/java/com/unittest/codecoverage/service/PersonValidatorTest.java
@@ -0,0 +1,24 @@
+package com.unittest.codecoverage.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.unittest.codecoverage.models.validators.PersonValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PersonValidatorTest {
+
+    private PersonValidator personValidator;
+
+    @BeforeEach
+    public void setUp() {
+        personValidator = new PersonValidator();
+    }
+
+    @Test
+    public void testRequiredNameWithString() {
+        assertTrue(personValidator.requiredName("John Doe"));
+        assertFalse(personValidator.requiredName(""));
+        assertFalse(personValidator.requiredName("   "));
+    }
+}
